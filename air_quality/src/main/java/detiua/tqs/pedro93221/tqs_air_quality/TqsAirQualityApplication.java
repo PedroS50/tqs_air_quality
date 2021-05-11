141befdf2a6c566834bd5d16626f7d7c1f288aff
Pedro Santos <pedro.miguel50@ua.pt>
2021-05-11 15:13:23 +0100

    Initial project structure.

---
+++ air_quality/src/main/java/detiua/tqs/pedro93221/tqs_air_quality/TqsAirQualityApplication.java
@@ -0,0 +1,13 @@
+package detiua.tqs.pedro93221.air_quality;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TqsAirQualityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TqsAirQualityApplication.class, args);
+	}
+
+}
---
+++ air_quality/src/test/java/detiua/tqs/pedro93221/tqs_air_quality/TqsAirQualityApplicationTests.java
@@ -0,0 +1,13 @@
+package detiua.tqs.pedro93221.air_quality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TqsAirQualityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
