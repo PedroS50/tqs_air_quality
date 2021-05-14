5327a01e528e712ab4bcc446e5b5665c646fd69e
Pedro Santos <pedro.miguel50@ua.pt>
2021-05-14 23:22:34 +0100

    Added demo, report. Minor changes to tests.

--- air_quality/src/main/java/detiua/tqs/pedro93221/air_quality/controller/AirPollutionRestController.java
+++ air_quality/src/main/java/detiua/tqs/pedro93221/air_quality/controller/AirPollutionRestController.java
@@ -15,7 +15,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @RestController
-@CrossOrigin(origins="http://localhost:4200/")
+@CrossOrigin(origins="*")
 @RequestMapping("/api")
 public class AirPollutionRestController {
 
--- air_quality/src/test/java/detiua/tqs/pedro93221/air_quality/webpage/ResultsPage.java
+++ air_quality/src/test/java/detiua/tqs/pedro93221/air_quality/webpage/ResultsPage.java
@@ -1,12 +1,11 @@
 package detiua.tqs.pedro93221.air_quality.webpage;
 
-import org.openqa.selenium.By;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ResultsPage {
 
@@ -34,8 +33,11 @@ public class ResultsPage {
     }
 
     public String getLocation() {
-        WebDriverWait wait = new WebDriverWait(driver, 60);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("locationHeader")));
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return locationHeader.getText();
     }
 
